ff592399f9971664db169f2ecd3c236aad5d3480
Samuel <samuel.weber@univ-grenoble-alpes.fr>
2021-03-17 16:37:52 +0100

    bump version

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ copyright = '2019, Samuël Weber'
 author = 'Samuël Weber'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 from os import path
 
-VERSION = '0.0.2'
+VERSION = '0.1.0'
 
 PACKAGES = [
         'pyPMF',
